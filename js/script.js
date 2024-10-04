b1b775a7276789c516618cc5fa60b838b5ee37ad
Zakin Ghani <zakinghani1@gmail.com>
2024-10-04 18:41:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// ======================== Doms Start Here ========================
+// ====== Player One Doms ======
+let errormsg1           = document.querySelector('.error1');
+let playerOne         = document.querySelector('.playerOne');
+let playerOneInput    = document.querySelector('.playerOneInput');
+let playerOneButton   = document.querySelector('.playerOneButton');
+// ====== Player Two Doms ======
+let errormsg2           = document.querySelector('.error2');
+let playerTwo         = document.querySelector('.playerTwo');
+let playerTwoInput    = document.querySelector('.playerTwoInput');
+let playerTwoButton   = document.querySelector('.playerTwoButton');
+// ====== Other Doms ======
+let chanceCount       = 1;
+let chanceleft        = document.querySelector('.chanceleft');
+let winner1           = document.querySelector('.winner1');
+let winner2           = document.querySelector('.winner2');
+// ======================== Doms End Here ========================
+
+// ==== Player One Code Start Here ====
+function handleplayerOne() {
+    if (playerOneInput.value == "" || playerOneInput.value < 1 || playerOneInput.value > 10) {
+        errormsg1.style.color = 'red';
+    } else {
+        errormsg1.style.color = '#fff';
+        playerTwo.style.display = 'flex';
+        playerOne.style.display = 'none';
+        errormsg1.innerHTML = '';
+        errormsg2.innerHTML = 'Guess the number between 1 - 10';
+    }
+}
+// ==== Player One Code End Here ====
+
+// ==== Player Two Code Start Here ====
+function handleplayerTwo() {
+    if (playerTwoInput.value == "" || playerTwoInput.value < 1 || playerTwoInput.value > 10) {
+        errormsg2.style.color = 'red';
+    } else {
+        if (playerTwoInput.value == playerOneInput.value) {
+            errormsg2.innerHTML = '';
+            playerTwo.style.display = 'none';
+            winner2.style.display = 'flex';
+        } else {
+            let chances = chanceCount++;
+            let totalChance = chanceleft.innerHTML = chances;
+            if (totalChance == 3) {
+                errormsg2.innerHTML = '';
+                playerTwo.style.display = 'none';
+                winner1.style.display = 'flex';
+            }
+        }
+    }
+}
+// ==== Player Two Code End Here ====
+
+// ==== Global Event Listeners ====
+playerOneButton.addEventListener('click', handleplayerOne);
+playerTwoButton.addEventListener('click', handleplayerTwo);
+
+function player1HandleInput(item) {
+    if (item.key == 'Enter') {
+        handleplayerOne();
+    }
+}
+
+function player2HandleInput(item) {
+    if (item.key == 'Enter') {
+        handleplayerTwo();
+    }
+}
